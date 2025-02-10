fc662411d18b097e53e4d9ebaf098b604dc76a9a
davidwells24 <136669947+davidwells24@users.noreply.github.com>
2025-02-10 07:53:58 +0000

    more html/css and short code added

--- .eleventy.js
+++ .eleventy.js
@@ -4,5 +4,12 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
 
-  
+  eleventyConfig.addShortcode("card", function(name, au_distance) {
+    return `<div class="planet">
+    <div class="card_wrapper no_hover">
+      <h3 class="planet_name">${name}</h3>
+      <p class="au_distance">${au_distance}</p>
+    </div>
+  </div>`;
+  });
 };
