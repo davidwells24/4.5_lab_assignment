42084db04d058b606800d9019b55494db6b47e3f
davidwells24 <136669947+davidwells24@users.noreply.github.com>
2025-02-11 00:31:02 +0000

    Modified eleventy shortcode and added planets

--- .eleventy.js
+++ .eleventy.js
@@ -4,9 +4,11 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
 
-  eleventyConfig.addShortcode("card", function(name, au_distance) {
+  eleventyConfig.addShortcode("card", function(planet_width, planet_height, margin_top, margin_bottom, color, name, au_distance) {
     return `<div class="planet">
     <div class="card_wrapper no_hover">
+      <div class="circle" style="width: ${planet_width}; height: ${planet_height}; margin-top: ${margin_top}; margin-bottom: ${margin_bottom}; background-color: ${color};">
+      </div>
       <h3 class="planet_name">${name}</h3>
       <p class="au_distance">${au_distance}</p>
     </div>
